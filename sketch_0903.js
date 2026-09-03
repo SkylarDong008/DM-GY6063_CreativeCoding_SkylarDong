6e99fd1303c093ecd4736e673cbdf49c80171733
Skylar Dong <cd3984@nyu.edu>
2026-09-03 16:52:09 -0400

    drawing

---
+++ sketch_0903.js
@@ -0,0 +1,17 @@
+function setup(){
+    createCanvas(w, h, [renderer])
+    // background(color, [a]) 
+    // these are top functions that runs over and over again
+
+}
+
+
+function draw(){
+    background(color, [a])
+    circle(mouseX, mouseY, 70) //注意顺序 会影响visual effect
+    noStroke()
+
+
+    console.log()
+
+}
